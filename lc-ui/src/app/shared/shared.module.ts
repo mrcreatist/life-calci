0490543f6d804947c224d52f35e35f54ca58951a
Abhishek Verma <ahk.verma102@gmail.com>
2022-03-30 10:46:20 +0530

    fixed shared

--- lc-ui/src/app/shared/shared.module.ts
+++ lc-ui/src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { service } from '../declaration';
+import { service } from './declaration';
 
 @NgModule({
   imports: [
